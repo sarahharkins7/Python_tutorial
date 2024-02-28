7d374a8f818a4b0898b2ea130058307193681947
Christopher Strickland <cstric12@utk.edu>
2024-02-27 19:52:30 -0500

    Updates to basics, scripting

--- scripting.py
+++ scripting.py
@@ -18,14 +18,15 @@ Email: cstric12@utk.edu
 # You will save yourself untold amounts of grief if you remember these words:
 #   "We write code to be read by humans, not machines."
 
-# Place all imports at the top of your script. Import only the stuff you need
-#   (and do not import *; It clutters the namespace and can lead to hard to 
-#   debug errors).
+# Place all imports at the top of your script. Import only the stuff you need.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.random import default_rng # you can import specific things
 
-# Here are some more examples of flow control:
+
+
+# Here are some more examples of flow control, using a while loop and if/elif/else.
 done_flag = False
 while not done_flag:
     your_number_txt = input('Input an even number greater than 6: ') # get some user input
@@ -34,10 +35,28 @@ while not done_flag:
     if your_number > 6 and your_number % 2 == 0: # % is the modulus operator
         print('Happy land!')
         done_flag = True # this will cause us to break out of the while loop
+        # could also have done a "while True" loop, and used "break" to break out
     elif your_number > 6 and not your_number % 2 == 0:
         print('The number {} is not even.'.format(your_number))
     else:
         # Note that to get the word It's, I use double quotations around the string
         print("{} is a silly number. It's just not large enough!".format(your_number))
 
-print(' ') # add an empty line after the prompt stuff
+
+
+# Now let's do an example where we plot something based on user input
+print('Plotting B + Asin(wt).')
+B = float(input('B: '))
+A = float(input('A: '))
+w = float(input('w: '))
+
+xmesh = np.linspace(-10,10,200) # Use linspace to quickly get an evenly spaced
+                                 #   mesh of values.
+ymesh = B + A*np.sin(xmesh) # All happens elementwise on the array xmesh
+
+plt.plot(xmesh,ymesh,label='sine')
+plt.legend() # Create a legend using the label entered in the plot command
+plt.title('Plot of sine')
+plt.xlabel('t')
+plt.show() # This must be there to show the plot and stop the program while
+           #    you examine it.
